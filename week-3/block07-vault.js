eafe4dd414582b352577a58afa7b0f2ab9f21c1d
Syaz S <syazski@gmail.com>
2023-10-16 20:44:55 -0500

    some exercises and notes

---
+++ week-3/block07-vault.js
@@ -0,0 +1,16 @@
+// show users an alert message about the combination
+//declare first variable using values that equals 10
+//declare second variable using values that equals 40
+//declare third variable using values that equals to 39
+//console.log() or alert() the entire combo
+
+
+alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination:");
+
+const firstCombo = 5 + 5;
+const secondCombo = 8 * 5;
+const thirdCombo = 40 - 1;
+const comboFinal = `${firstCombo} ${secondCombo} ${thirdCombo}`;
+// another way to to do this instead of creating another variable is to console.log (firstCombo, secondCombo, thirdCombo);
+
+alert(comboFinal);
---
+++ week-3/guided-practice.js
@@ -0,0 +1,28 @@
+// Grab toothbrush, turn water, wash brush 
+// turn off water, open tube, squeeze tube, insert toothbrush in mouth, start scrubbing teeth
+
+const numberofCows = 50;
+
+const greeting = "Hello World";
+
+console.log(greeting);
+
+console.log("Hello" + "Disney Land!");
+
+const umbrella = 10 * 3;
+
+const num = 5 + 1;
+console.log(num);
+
+<script src="guided-practice.js"></script>
+
+const str = 'Hello' + ' ' + 'World';
+
+let stringHello = "Hello,";
+let stringI = "I am";
+const name = "Josh";
+const stringAlert = `${stringHello} ${stringI} ${name}`;
+alert(stringAlert);
+
+//note: another way to do this is just to console.log (stringHello, stringI, name) 
+
